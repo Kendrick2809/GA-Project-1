6628bb4222cc6c59e569259562fb5a5ef4195cb0
Kendrick Winata <kendrickwinata@Najwas-MacBook-Pro-2.local>
2022-07-02 11:17:25 +0800

    update

--- main.js
+++ main.js
@@ -92,7 +92,7 @@ axios
       if (
         window.location.pathname ==
           "/Users/kendrickwinata/GA-Project-1/index.html" ||
-        window.location.pathname == "/GA-Project-1/index.html"
+        window.location.pathname == "/GA-Project-1/"
       ) {
         //add index row to DOM
         for (let i = 0; i < indexArrayLength; i++) {
@@ -135,7 +135,7 @@ axios
       if (
         window.location.pathname ==
           "/Users/kendrickwinata/GA-Project-1/portfolio.html" ||
-        window.location.pathname == "/GA-Project-1/portfolio.html"
+        window.location.pathname == "/GA-Project-1/"
       ) {
         //parse the portfolio item
         const transferredPortfolioData = JSON.parse(
